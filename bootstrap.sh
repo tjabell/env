450bf11d25d33b3d395cf92f75f33d4b6dbaa8d6
Trevor Abell <trevor.abell@parsus.com>
2016-01-05 21:39:28 -0700

    added basic bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm ~/.bashrc
+ln -s ~/env/bashbootstrap ~/.bashrc
+rm ~/.tmux.conf
+ln -s ~/env/tmux.conf ~/.tmux.conf
+rm ~/.gitconfig
+ln -s ~/env/gitconfig ~/.gitconfig
+git clone https://tjabell@github.com/tjabell/emacs.git ~/emacs
+~/run-packages.sh
+
