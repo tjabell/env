fd30f94e196a7a2e952d12cd4d69a085eb47fb04
Trevor Abell <trevor@keikocode.com>
2016-01-07 20:38:54 -0700

    added pull for org repo

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 git -C /home/trevor/env/ pull
 git -C /home/trevor/emacs/ pull
+git -C /home/trevor/org/ pull
 sh /home/trevor/emacs/run-packages.sh
 systemctl --user restart emacs
