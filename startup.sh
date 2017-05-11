c082d09716d6ec1815e89522cab56f9b55f8e70a
Trevor Abell <trevor.abell@parsus.com>
2017-05-11 15:33:43 -0700

    Add check for ssh key already added to agent

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-ssh-add
+fp=`ssh-keygen -lf ~/.ssh/id_rsa.pub | awk '{print $2}'`
+keys=`ssh-add -l`
+if [[ ! $keys == *"$fp"* ]]; then
+    ssh-add
+fi
 git -C /home/trevor/env/ pull
 git -C /home/trevor/emacs/ pull
 git -C /home/trevor/org/ pull
