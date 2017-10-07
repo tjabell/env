b3e4c413c1904f0faae9fdc12954f51817ed06a1
Trevor Abell <trevor@keikocode.com>
2017-10-07 11:57:55 -0700

    fork git jobs

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ keys=`ssh-add -l`
 if [[ ! $keys == *"$fp"* ]]; then
     ssh-add
 fi
-git -C /home/trevor/env/ pull
-git -C /home/trevor/emacs/ pull
-git -C /home/trevor/org/ pull
+git -C /home/trevor/env/ pull &
+git -C /home/trevor/emacs/ pull &
+git -C /home/trevor/org/ pull &
 
