a7508d8eb5f6eafd01572a00fdb17f2cb489e7e9
Trevor Abell <trevor.abell@parsus.com>
2018-04-08 08:26:10 -0700

    Add rebase to repository sync

--- startup.sh
+++ startup.sh
@@ -4,16 +4,16 @@ keys=`ssh-add -l`
 if [[ ! $keys == *"$fp"* ]]; then
     ssh-add
 fi
-git -C /home/trevor/env/ pull &
+git -C /home/trevor/env/ pull --rebase &
 
 if [[ -d /home/trevor/emacs/ ]]
 then
-git -C /home/trevor/emacs/ pull &    
+git -C /home/trevor/emacs/ pull --rebase &    
 fi
 
 if [[ -d /home/trevor/org/ ]]
 then
-    git -C /home/trevor/org/ pull &
+    git -C /home/trevor/org/ pull --rebase &
 fi
 
 
