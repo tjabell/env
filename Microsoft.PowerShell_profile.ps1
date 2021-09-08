7d027269f98a3a1af5709dfa0405bf17669e9f19
Trevor Abell <trevor.abell@parsus.com>
2021-09-08 11:13:53 -0500

    windows/add powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,92 @@
+function cds {
+    cd C:\Users\trevor\source\
+}
+function cdsk {
+    cd C:\Users\trevor\source\karisma\karisma.umbraco-website\Karisma.Website.UmbracoWeb\
+}
+
+# cds = cd source directory
+function cdse {
+    cd C:\Users\trevor\source\wsl\equinox\
+}
+
+
+function cdsa {
+    cd C:\Users\trevor\source\wsl\acdhh\
+}
+
+function cdsac {
+    cd C:\Users\trevor\source\wsl\acdhh\cdbms
+}
+
+function cdsacw {
+    cd C:\Users\trevor\source\wsl\acdhh\cdbms\ACDHH.Web
+}
+
+function cdsaw {
+    cd C:\Users\trevor\source\wsl\acdhh\acdhh.public-website
+}
+
+function cdsew {
+    cd C:\Users\trevor\source\wsl\equinox\equinox-rki.wpt\
+}
+
+function cdse {
+    cd C:\Users\trevor\source\wsl\equinox\
+}
+
+function cdser {
+    cd C:\Users\trevor\source\wsl\equinox\equinox-rki.rks\
+}
+
+function cdserw {
+    cd C:\Users\trevor\source\wsl\equinox\equinox-rki.rks\HRKS.Web\
+}
+
+# cdr = cd release directory
+function cdr {
+    # cd acdhh cdbms release direcotry (ACDHH.Web)
+    cd C:\Users\trevor\source\
+}
+
+function cdra {
+    # cd acdhh cdbms release direcotry (ACDHH.Web)
+    cd C:\Users\trevor\source\acdhh\
+}
+
+function cdrac {
+    # cd acdhh cdbms release direcotry (ACDHH.Web)
+    cd C:\Users\trevor\source\acdhh\cdbms\ACDHH.Web
+}
+
+function cdraw {
+    # cd acdhh public website release direcotry (www.acdhh.org)
+    cd C:\Users\trevor\source\acdhh\acdhh.public-website\src\Acdhh.Website.WebApp\
+}
+
+function cdracw {
+    # cd acdhh cdbms release direcotry (ACDHH.Web)
+    cd C:\Users\trevor\source\acdhh\cdbms\ACDHH.Web
+}
+
+function cdre {
+    cd C:\Users\trevor\source\equinox\
+}
+
+# use wsl for this one since there aren't really any deploy directory diffs
+function cdrer {
+    cd C:\Users\trevor\source\wsl\equinox\equinox-rki.rks\
+}
+
+# use wsl for this one since there aren't really any deploy directory diffs
+function cdrerw {
+    cd C:\Users\trevor\source\wsl\equinox\equinox-rki.rks\HRKS.Web\
+}
+
+function cdrew {
+    cd C:\Users\trevor\source\equinox\equinox-rki.wpt\
+}
+
+function cdrewa {
+    cd C:\Users\trevor\source\equinox\equinox-rki.wpt\Activation
+}
