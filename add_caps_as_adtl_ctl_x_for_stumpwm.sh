c30339d6ef158dbe9ef828c1fe19e6729c07ca12
Trevor Abell <trevor.abell@parsus.com>
2021-09-08 11:13:48 -0500

    add some stumpwm config

---
+++ add_caps_as_adtl_ctl_x_for_stumpwm.sh
@@ -0,0 +1,6 @@
+xmodmap -e 'remove Lock = Caps_Lock'
+xmodmap -e 'remove Control = Control_L'
+xmodmap -e 'keysym Control_L = Caps_Lock'
+xmodmap -e 'keysym Caps_Lock = Control_L'
+xmodmap -e 'add Lock = Caps_Lock'
+xmodmap -e 'add Control = Control_L'
