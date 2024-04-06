8a8ddad108235f461eeb699985f10e9746b71b4a
Trevor Abell <tjabell@gmail.com>
2024-04-07 05:50:17 +1000

    beancount comments

--- tools/start-beancount-import.sh
+++ tools/start-beancount-import.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+#Need to run  `source /home/trevor/sl/beancount-import/bin/activate` first
 source /home/trevor/sl/beancount-import/bin/activate
 cd /home/trevor/personal/office/beancount-import/
 python ./run.py
